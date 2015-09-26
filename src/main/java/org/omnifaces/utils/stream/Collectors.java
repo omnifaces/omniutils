3dd4e779862fe66ce6685fc6a7edb65b10d4d712
Jan Beernink <jan.beernink@gmail.com>
2015-09-26 16:04:16 +0200

    Implemented combined collector

--- src/main/java/org/omnifaces/utils/stream/Collectors.java
+++ src/main/java/org/omnifaces/utils/stream/Collectors.java
@@ -1,6 +1,7 @@
 package org.omnifaces.utils.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collector;
@@ -16,6 +17,10 @@ public final class Collectors {
 
 	public static <T> Collector<T, ?, Void> forEachBatch(Consumer<List<T>> batchConsumer, int batchSize) {
 		return new ForEachBatchCollector<>(batchConsumer, batchSize);
+	}
+
+	public static <T, R1, R2> Collector<T, ?, Map.Entry<R1, R2>> combine(Collector<T, ?, R1> collector1, Collector<T, ?, R2> collector2) {
+		return new CombinedCollector<>(collector1, collector2);
 	}
 
 }
