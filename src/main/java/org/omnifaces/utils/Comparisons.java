17ac9b5ec9848226e6843a6d0f30c02a36a55bfb
Jan Beernink <jan.beernink@gmail.com>
2016-09-10 22:18:02 +0200

    Add method to get the max and min of two objects, based on a given ordering

---
+++ src/main/java/org/omnifaces/utils/Comparisons.java
@@ -0,0 +1,35 @@
+package org.omnifaces.utils;
+
+import java.util.Comparator;
+
+import static java.util.Comparator.naturalOrder;
+
+public final class Comparisons {
+
+    private Comparisons() {}
+
+    public static <T extends Comparable<T>> T min(T t1, T t2) {
+        return min(t1, t2, naturalOrder());
+    }
+
+    public static <T> T min(T t1, T t2, Comparator<? super T> comparator) {
+        if (comparator.compare(t1, t2) <= 0) {
+            return t1;
+        }
+
+        return t2;
+    }
+
+    public static <T extends Comparable<T>> T max(T t1, T t2) {
+        return max(t1, t2, naturalOrder());
+    }
+
+    public static <T> T max(T t1, T t2, Comparator<? super T> comparator) {
+        if (comparator.compare(t1, t2) >= 0) {
+            return t1;
+        }
+
+        return t2;
+    }
+
+}
