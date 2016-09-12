6a4b639428c92db7fe4763ba30327ce476c4aed6
Jan Beernink <jan.beernink@gmail.com>
2016-09-12 20:58:27 +0200

    Add varargs min and max methods

--- src/main/java/org/omnifaces/utils/Comparisons.java
+++ src/main/java/org/omnifaces/utils/Comparisons.java
@@ -10,10 +10,14 @@ public final class Comparisons {
 	}
 
 	public static <T extends Comparable<T>> T min(T t1, T t2) {
-		return min(t1, t2, naturalOrder());
+		return min(naturalOrder(), t1, t2);
 	}
 
-	public static <T> T min(T t1, T t2, Comparator<? super T> comparator) {
+	public static <T extends Comparable<T>> T min(T t1, T t2, T... ts) {
+		return min(naturalOrder(), t1, t2, ts);
+	}
+
+	public static <T> T min(Comparator<? super T> comparator, T t1, T t2) {
 		if (comparator.compare(t1, t2) <= 0) {
 			return t1;
 		}
@@ -21,16 +25,40 @@ public final class Comparisons {
 		return t2;
 	}
 
-	public static <T extends Comparable<T>> T max(T t1, T t2) {
-		return max(t1, t2, naturalOrder());
+	private static <T extends Comparable<T>> T min(Comparator<T> tComparator, T t1, T t2, T... ts) {
+		T min = min(tComparator, t1, t2);
+
+		for (T t : ts) {
+			min = min(tComparator, min, t);
+		}
+
+		return min;
 	}
 
-	public static <T> T max(T t1, T t2, Comparator<? super T> comparator) {
+	public static <T extends Comparable<T>> T max(T t1, T t2) {
+		return max(naturalOrder(), t1, t2);
+	}
+
+	public static <T extends Comparable<T>> T max(T t1, T t2, T... ts) {
+		return max(naturalOrder(), t1, t2, ts);
+	}
+
+	public static <T> T max(Comparator<? super T> comparator, T t1, T t2) {
 		if (comparator.compare(t1, t2) >= 0) {
 			return t1;
 		}
 
 		return t2;
+	}
+
+	private static <T extends Comparable<T>> T max(Comparator<T> comparator, T t1, T t2, T... ts) {
+		T max = max(comparator, t1, t2);
+
+		for (T t : ts) {
+			max = max(comparator, max, t);
+		}
+
+		return max;
 	}
 
 }
--- src/test/java/org/omnifaces/utils/ComparisonsTest.java
+++ src/test/java/org/omnifaces/utils/ComparisonsTest.java
@@ -14,13 +14,15 @@ public class ComparisonsTest {
 		assertEquals(1L, (long) min(1, 2));
 		assertEquals(1L, (long) min(2, 1));
 		assertEquals(2L, (long) min(2, 2));
+
+		assertEquals(2L, (long) min(4, 3, 2));
 	}
 
 	@Test
 	public void testMinWithComparator() {
-		assertEquals(2L, (long) min(1, 2, reverseOrder()));
-		assertEquals(2L, (long) min(2, 1, reverseOrder()));
-		assertEquals(2L, (long) min(2, 2, reverseOrder()));
+		assertEquals(2L, (long) min(reverseOrder(), 1, 2));
+		assertEquals(2L, (long) min(reverseOrder(), 2, 1));
+		assertEquals(2L, (long) min(reverseOrder(), 2, 2));
 	}
 
 	@Test
@@ -28,13 +30,15 @@ public class ComparisonsTest {
 		assertEquals(2L, (long) max(1, 2));
 		assertEquals(2L, (long) max(2, 1));
 		assertEquals(2L, (long) max(2, 2));
+
+		assertEquals(2L, (long) max(0, 1, 2));
 	}
 
 	@Test
 	public void testMaxWithComparator() {
-		assertEquals(1L, (long) max(1, 2, reverseOrder()));
-		assertEquals(1L, (long) max(2, 1, reverseOrder()));
-		assertEquals(2L, (long) max(2, 2, reverseOrder()));
+		assertEquals(1L, (long) max(reverseOrder(), 1, 2));
+		assertEquals(1L, (long) max(reverseOrder(), 2, 1));
+		assertEquals(2L, (long) max(reverseOrder(), 2, 2));
 	}
 
 }
