6ef2d5559fceb48ae64b4901386e4dde798a61a7
Jan Beernink <jan.beernink@gmail.com>
2016-09-12 10:15:15 +0200

    Fix formatting

--- src/main/java/org/omnifaces/utils/Comparisons.java
+++ src/main/java/org/omnifaces/utils/Comparisons.java
@@ -1,35 +1,36 @@
 package org.omnifaces.utils;
 
-import java.util.Comparator;
-
 import static java.util.Comparator.naturalOrder;
+
+import java.util.Comparator;
 
 public final class Comparisons {
 
-    private Comparisons() {}
+	private Comparisons() {
+	}
 
-    public static <T extends Comparable<T>> T min(T t1, T t2) {
-        return min(t1, t2, naturalOrder());
-    }
+	public static <T extends Comparable<T>> T min(T t1, T t2) {
+		return min(t1, t2, naturalOrder());
+	}
 
-    public static <T> T min(T t1, T t2, Comparator<? super T> comparator) {
-        if (comparator.compare(t1, t2) <= 0) {
-            return t1;
-        }
+	public static <T> T min(T t1, T t2, Comparator<? super T> comparator) {
+		if (comparator.compare(t1, t2) <= 0) {
+			return t1;
+		}
 
-        return t2;
-    }
+		return t2;
+	}
 
-    public static <T extends Comparable<T>> T max(T t1, T t2) {
-        return max(t1, t2, naturalOrder());
-    }
+	public static <T extends Comparable<T>> T max(T t1, T t2) {
+		return max(t1, t2, naturalOrder());
+	}
 
-    public static <T> T max(T t1, T t2, Comparator<? super T> comparator) {
-        if (comparator.compare(t1, t2) >= 0) {
-            return t1;
-        }
+	public static <T> T max(T t1, T t2, Comparator<? super T> comparator) {
+		if (comparator.compare(t1, t2) >= 0) {
+			return t1;
+		}
 
-        return t2;
-    }
+		return t2;
+	}
 
 }
--- src/test/java/org/omnifaces/utils/ComparisonsTest.java
+++ src/test/java/org/omnifaces/utils/ComparisonsTest.java
@@ -1,40 +1,40 @@
 package org.omnifaces.utils;
 
-import org.junit.Test;
-
 import static java.util.Comparator.reverseOrder;
 import static org.junit.Assert.assertEquals;
 import static org.omnifaces.utils.Comparisons.max;
 import static org.omnifaces.utils.Comparisons.min;
 
+import org.junit.Test;
+
 public class ComparisonsTest {
 
-    @Test
-    public void testMin() {
-        assertEquals(1L, (long) min(1, 2));
-        assertEquals(1L, (long) min(2, 1));
-        assertEquals(2L, (long) min(2, 2));
-    }
+	@Test
+	public void testMin() {
+		assertEquals(1L, (long) min(1, 2));
+		assertEquals(1L, (long) min(2, 1));
+		assertEquals(2L, (long) min(2, 2));
+	}
 
-    @Test
-    public void testMinWithComparator() {
-        assertEquals(2L, (long) min(1, 2, reverseOrder()));
-        assertEquals(2L, (long) min(2, 1, reverseOrder()));
-        assertEquals(2L, (long) min(2, 2, reverseOrder()));
-    }
+	@Test
+	public void testMinWithComparator() {
+		assertEquals(2L, (long) min(1, 2, reverseOrder()));
+		assertEquals(2L, (long) min(2, 1, reverseOrder()));
+		assertEquals(2L, (long) min(2, 2, reverseOrder()));
+	}
 
-    @Test
-    public void testMax() {
-        assertEquals(2L, (long) max(1, 2));
-        assertEquals(2L, (long) max(2, 1));
-        assertEquals(2L, (long) max(2, 2));
-    }
+	@Test
+	public void testMax() {
+		assertEquals(2L, (long) max(1, 2));
+		assertEquals(2L, (long) max(2, 1));
+		assertEquals(2L, (long) max(2, 2));
+	}
 
-    @Test
-    public void testMaxWithComparator() {
-        assertEquals(1L, (long) max(1, 2, reverseOrder()));
-        assertEquals(1L, (long) max(2, 1, reverseOrder()));
-        assertEquals(2L, (long) max(2, 2, reverseOrder()));
-    }
+	@Test
+	public void testMaxWithComparator() {
+		assertEquals(1L, (long) max(1, 2, reverseOrder()));
+		assertEquals(1L, (long) max(2, 1, reverseOrder()));
+		assertEquals(2L, (long) max(2, 2, reverseOrder()));
+	}
 
 }
